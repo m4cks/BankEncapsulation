4bd80336bb2b23f8907e0f9376f26c318faaeb3f
Max Schlapfer <max.schlapfer@gmail.com>
2022-09-29 10:11:15 -0500

    initial commit

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankEncapsulation
+{
+    internal class BankAccount
+    {
+        private double balance = 0;
+
+        public void Deposit(double val)
+        {
+            balance += val;
+        }
+
+        public double GetBalance()
+        {
+            return balance;
+        }
+    }
+}
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account = new BankAccount();
+            Console.WriteLine($"Current account balance: {account.GetBalance()}");
+            double deposit = 0;
+            while(true)
+            {
+                Console.WriteLine("Make a deposit by typing a number!");
+                try
+                {
+                    deposit = Int32.Parse(Console.ReadLine());
+                } catch
+                {
+                    Console.WriteLine("If you don't want to type a valid number format, I'm just gonna close the program! Ha!");
+                    break;
+                }
+                account.Deposit(deposit);
+                Console.WriteLine($"Nice! Your new account balance is: {account.GetBalance()}");
+            }
+        }
+    }
+}
